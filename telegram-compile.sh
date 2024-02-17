b5897037db47e46ed01503b49fc8911c0235f31e
Ragil <work.ragil@gmail.com>
2024-02-18 02:08:28 +0800

    a

--- telegram-compile.sh
+++
@@ -1,2 +0,0 @@
-curl -s -X POST https://api.telegram.org/bot${TOKEN}/sendMessage -d chat_id=${CHAT_ID} -d parse_mode="HTML" -d text="<b>Project</b> : Hackathon BCA by HAI MP 06                     
-<b>Status </b> : Success - Compile Changes to Web App Nodejs"
